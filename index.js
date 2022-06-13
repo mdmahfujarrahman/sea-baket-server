425b5034df14aed6d3b98f4caa61bf0e80bf7bc5
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-06-13 22:36:10 +0600

    mongodb conencted

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zemxo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run (){
+
+    
+    client.connect((err) => {
+        const collection = client.db("test").collection("devices");
+        console.log('mongodb connected');
+        client.close();
+    });
+
+
+
+
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
